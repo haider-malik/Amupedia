4f19d86cf1f99fdf42804431d40ef18fe41306b3
Haider Ali <malikhaider0567@gmail.com>
2021-10-21 14:35:28 +0530

    BodyParser Depricated

--- 0_backend/index.js
+++ 0_backend/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const connectToMongo = require('./db');
+const express = require("express");
+const connectToMongo = require("./db");
 const path = require("path");
-const Form = require('./models/Form');
+const Form = require("./models/Form");
 const bodyParser = require("body-parser");
 
 connectToMongo();
@@ -9,28 +9,28 @@ connectToMongo();
 const port = 80;
 const app = express();
 
+app.use(bodyParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../Home/home.html'));
-})
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "../Home/home.html"));
+});
 
-app.post('/', async (req, res) => {
-  let form = await Form.create({
-    firstname: req.body.firstname,
-    lastname: req.body.lastname,
-    email: req.body.email,
-    phone: req.body.phone,
-    address: req.body.address,
-    message: req.body.message,
-  });
-  console.log(form);
-  form.save();
-  res.sendFile(path.join(__dirname, '../Home/home.html'));
-})
-
+app.post("/", async (req, res) => {
+	let form = await Form.create({
+		firstname: req.body.firstname,
+		lastname: req.body.lastname,
+		email: req.body.email,
+		phone: req.body.phone,
+		address: req.body.address,
+		message: req.body.message,
+	});
+	console.log(form);
+	form.save();
+	res.sendFile(path.join(__dirname, "../Home/home.html"));
+});
 
 app.listen(port, () => {
-  console.log(`App listening on http://localhost:80`);
-})
+	console.log(`App listening on http://localhost:80`);
+});
